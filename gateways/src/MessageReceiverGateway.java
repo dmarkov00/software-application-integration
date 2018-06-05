507dfbc6fa4db4cb6a3041a0776ca05a8362e91a
Jack D <dmarkov23@gmail.com>
2018-06-05 19:28:54 +0200

    created base gateway classes

---
+++ gateways/src/MessageReceiverGateway.java
@@ -0,0 +1,16 @@
+import org.apache.camel.Consumer;
+
+import javax.jms.*;
+
+public class MessageReceiverGateway {
+
+    private Connection connection;
+    private Session session;
+    private Destination destination;
+    private Consumer consumer;
+    private String channelName;
+
+    public void MessageReceiverGateway(String channelName) {
+        this.channelName = channelName;
+    }
+}
---
+++ gateways/src/MessageSenderGateway.java
@@ -0,0 +1,28 @@
+import org.apache.camel.Producer;
+
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.*;
+
+public class MessageSenderGateway {
+    private Connection connection;
+    private Session session;
+    private Destination destination;
+    private Producer producer;
+    private String channelName;
+
+    public MessageSenderGateway(String channelName) {
+        this.channelName = channelName;
+    }
+
+    public Message createTextMessage(String body) {
+        return null;
+    }
+
+    public void send(Message msg) {
+
+    }
+}
+
