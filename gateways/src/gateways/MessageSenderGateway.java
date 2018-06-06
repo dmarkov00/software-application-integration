a5f9b96e649dfef3a7fbfecad9302b39eef2f37f
Jack D <dmarkov23@gmail.com>
2018-06-06 11:37:45 +0200

    refactored message sender gateway to match the provided class diagram

--- gateways/src/gateways/MessageSenderGateway.java
+++ gateways/src/gateways/MessageSenderGateway.java
@@ -7,18 +7,39 @@ import javax.naming.NamingException;
 import java.util.*;
 
 public class MessageSenderGateway {
-    private Connection connection;
     private Session session;
-    private Destination sendDestination;
-    private Destination replyDestination;
     private MessageProducer producer;
-    private Properties props;
+    private Context jndiContext;
     private String replyQueue;
     private String requestQueue;
 
     public MessageSenderGateway(String requestQueue, String replyQueue) {
         this.requestQueue = requestQueue;
         this.replyQueue = replyQueue;
+
+        try {
+            // Setup context
+            Properties props = new Properties();
+            props.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+            props.setProperty(Context.PROVIDER_URL, "tcp://localhost:61616");
+
+            props.put(("queue." + requestQueue), requestQueue);
+            props.put(("queue." + replyQueue), replyQueue);
+
+            // Setup connection
+            ConnectionFactory connectionFactory = (ConnectionFactory) jndiContext
+                    .lookup("ConnectionFactory");
+            Connection connection = connectionFactory.createConnection();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            Destination sendDestination = (Destination) jndiContext.lookup(requestQueue);
+
+            producer = session.createProducer(sendDestination);
+
+            this.jndiContext = new InitialContext(props);
+        } catch (NamingException | JMSException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -26,26 +47,7 @@ public class MessageSenderGateway {
 
         Message msg = null;
         try {
-            props = new Properties();
-            props.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
-            props.setProperty(Context.PROVIDER_URL, "tcp://localhost:61616");
-
-            props.put(("queue." + requestQueue), requestQueue);
-            props.put(("queue." + replyQueue), replyQueue);
-
-            Context jndiContext = new InitialContext(props);
-
-            // Setup connection
-            ConnectionFactory connectionFactory = (ConnectionFactory) jndiContext
-                    .lookup("ConnectionFactory");
-            connection = connectionFactory.createConnection();
-            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-            // Setup destinations
-            sendDestination = (Destination) jndiContext.lookup(requestQueue);
-            replyDestination = (Destination) jndiContext.lookup(replyQueue);
-
-            producer = session.createProducer(sendDestination);
+            Destination replyDestination = (Destination) jndiContext.lookup(replyQueue);
 
             // Create a text message
             msg = session.createTextMessage(messageBody);
@@ -53,8 +55,6 @@ public class MessageSenderGateway {
             // Set message reply destination
             msg.setJMSReplyTo(replyDestination);
 
-            // Send the message
-            producer.send(msg);
 
         } catch (NamingException | JMSException e) {
             e.printStackTrace();
@@ -64,7 +64,13 @@ public class MessageSenderGateway {
     }
 
     public void send(Message msg) {
+        try {
+            // Send the message
+            producer.send(msg);
 
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
     }
 }
 
